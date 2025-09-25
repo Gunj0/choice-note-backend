06ea31fa6c6833a80d6bef5c26b7d170893d2040
Gunj0 <gunjo2012@gmail.com>
2025-09-26 00:39:13 +0900

    DB接続リトライ追加

--- ChoiceNote.WebAPI/Program.cs
+++ ChoiceNote.WebAPI/Program.cs
@@ -5,11 +5,19 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// PostgreSQL 用の DbContext/接続文字列 を登録
+// DbContext を PostgreSQL 用に登録
+// Scoped ライフタイムでリクエストごとに新しい DbContext インスタンスを使用する
 builder.Services.AddDbContext<ChoiceNoteDbContext>(options =>
     options.UseNpgsql(
         builder.Configuration.GetConnectionString("DefaultConnection")
-        ?? throw new InvalidOperationException("Connection string not found."))
+        ?? throw new InvalidOperationException("Connection string not found."),
+        npgsqlOptions =>
+        {
+            npgsqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 5, // 最大リトライ回数
+                maxRetryDelay: TimeSpan.FromSeconds(30), // リトライ間隔の最大値
+                errorCodesToAdd: null); // 追加でリトライ対象にするエラーコード
+        })
 );
 
 // コントローラの追加
